f23f5308a4304f1a34d86c8389bceda549f29890
vashnavichauhan1825 <70444778+vashnavichauhan1825@users.noreply.github.com>
2022-05-20 10:10:29 +0530

    add slider and styled components

---
+++ src/data.js
@@ -0,0 +1,25 @@
+export const sliderItems = [
+    {
+      bg: "000316",
+      id: 1,
+      img: "https://images.unsplash.com/photo-1607250267267-29ac1fdc4536?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+      title: "MOON LAMPS",
+      desc: "POPULAR ! GET FLAT 30% OFF FOR SPACE LOVERS.",
+    },
+    {
+      bg:"a9781f",
+      id: 2,
+      img: "https://images.unsplash.com/photo-1519913974306-4c20d7349ee5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+      title: "LAMPS LOVE",
+      desc: "DON'T COMPROMISE ON LIGHTS! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+     
+    },
+    {
+      bg:"04b8e5",
+      id: 3,
+      img: "https://images.unsplash.com/photo-1551029506-0807df4e2031?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1334&q=80",
+      title: "BOOKS SALE",
+      desc: "DON'T COMPROMISE ON BOOKS! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+     
+    },
+  ];
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar/Navbar'
+import Slider from '../components/Slider/Slider'
 import './Home.css'
 
 const Home = () => {
   return (
     <div>
         <Navbar/>
+        <Slider/>
     </div>
   )
 }
