13e4ea2d5801cfbcd83ee2f9490f9792dac94021
vashnavichauhan1825 <70444778+vashnavichauhan1825@users.noreply.github.com>
2022-05-25 21:29:44 +0530

    add footer

--- src/UI/wrapperNav/WrapperNav.js
+++ src/UI/wrapperNav/WrapperNav.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Fragment } from 'react/cjs/react.production.min'
-import { Navbar, Slider } from '../../components'
+import { Footer, Navbar, Slider } from '../../components'
 
  export const WrapperNav = ({children}) => {
   return (
     <Fragment>
       <Navbar/>
       {children}
+      <Footer/>
     </Fragment>
   )
 }
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -16,7 +16,6 @@ export const Home = () => {
         <ImgLayout/>
         <Reviews/>
         <Newsletter/>
-        <Footer/>
     </WrapperNav>
     
   )
