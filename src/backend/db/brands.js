dfad5e12cd5b44e502b66f7a70dcaeb72bf875c4
vashnavichauhan1825 <70444778+vashnavichauhan1825@users.noreply.github.com>
2022-05-26 12:43:50 +0530

    add data to backend db

---
+++ src/backend/db/brands.js
@@ -0,0 +1,29 @@
+import { v4 as uuid } from "uuid";
+
+/**
+ * Brand Database can be added here.
+ * You can add brand of your wish with different attributes
+ * */
+
+export const brands = [
+  {
+    _id: uuid(),
+    brandName: "celestron",
+    value: "celestron",
+  },
+  {
+    _id: uuid(),
+    brandName: "generic",
+    value: "generic",
+    },
+  {
+    _id: uuid(),
+    brandName: "meade",
+    value: "meade",
+  },
+  {
+    _id: uuid(),
+    brandName: "aomeike",
+    value: "aomeike",
+   },
+]
