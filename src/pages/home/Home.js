af89e6eb0f02a0de582c4a21a9ee6f0c7e9df37e
vashnavichauhan1825 <vashnavichauhan1@gmail.com>
2022-06-22 10:31:37 +0530

    Update Home.js

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,7 +3,6 @@ import { Toast } from 'components/Toast/Toast'
 import {FeatureProduct, ImgLayout,Newsletter,
   PopularCategory,Service,Reviews,Slider} from '../../components'
 import{ WrapperNav} from '../../UI'
-
 import './Home.css'
 
 export const Home = () => {
